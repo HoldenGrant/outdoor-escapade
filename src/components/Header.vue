979ca1123bf64fe9bef4d250d70131830c1ae803
Holden <holdengrantcausing@gmail.com>
2021-03-02 00:13:44 +0800

    nav

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,26 @@
 <template>
     <b-container>
         <b-row>
-        <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/contact-us">Contact Us</router-link>
-    </div>
+            <div id="nav" class="w-100 p-0">
+
+                <b-navbar toggleable="sm" type="light" variant="light">
+
+                    <b-navbar-toggle target="nav-text-collapse"></b-navbar-toggle>
+
+                    <b-navbar-brand>BootstrapVue</b-navbar-brand>
+
+                    <b-collapse id="nav-text-collapse" is-nav>
+
+                    <b-navbar-nav class="text-right ml-auto">
+                        <b-nav-item><router-link to="/">Home</router-link></b-nav-item>
+                        <b-nav-item><router-link to="/about">About</router-link></b-nav-item>
+                        <b-nav-item><router-link to="/contact-us">Contact Us</router-link></b-nav-item>
+                    </b-navbar-nav>
+                    </b-collapse>
+                    
+                </b-navbar>
+
+            </div>
         </b-row>
     </b-container>
 </template>
