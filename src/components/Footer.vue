8d19542522d100520fa30ae4c88f45113fd0ebd4
Holden <holdengrantcausing@gmail.com>
2021-03-01 23:46:34 +0800

    header-footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+    <b-container>
+        <b-row>
+        <h1>{{ msg }}</h1>
+        footer
+        </b-row>
+    </b-container>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+  props: {
+    msg: String
+  }
+}
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+    <b-container>
+        <b-row>
+        <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/contact-us">Contact Us</router-link>
+    </div>
+        </b-row>
+    </b-container>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    msg: String
+  }
+}
+</script>
