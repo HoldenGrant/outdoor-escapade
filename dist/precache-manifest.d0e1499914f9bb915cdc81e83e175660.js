c1c672c9f5cb3ee1369bdeee2daef33b5e627eb6
Holden <holdengrantcausing@gmail.com>
2021-03-02 15:24:07 +0800

    test dep

---
+++ dist/precache-manifest.d0e1499914f9bb915cdc81e83e175660.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d80b2c9d178c62edf5e",
+    "url": "/outdoor-escapade/css/app.09046491.css"
+  },
+  {
+    "revision": "a7af38739a9c601b6820",
+    "url": "/outdoor-escapade/css/chunk-vendors.559fc55d.css"
+  },
+  {
+    "revision": "4083d3d182354146f5fd875263c2f635",
+    "url": "/outdoor-escapade/img/camping-tent.4083d3d1.svg"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/outdoor-escapade/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "1c0d67fd3a595a0e998a71f9ef16198d",
+    "url": "/outdoor-escapade/index.html"
+  },
+  {
+    "revision": "4f44712b34544f8906eb",
+    "url": "/outdoor-escapade/js/about.c68c318c.js"
+  },
+  {
+    "revision": "6d80b2c9d178c62edf5e",
+    "url": "/outdoor-escapade/js/app.d93bc352.js"
+  },
+  {
+    "revision": "a7af38739a9c601b6820",
+    "url": "/outdoor-escapade/js/chunk-vendors.ceed5c81.js"
+  },
+  {
+    "revision": "ccf72d4a46a6115818d265b21c7d6e59",
+    "url": "/outdoor-escapade/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/outdoor-escapade/robots.txt"
+  }
+]);
